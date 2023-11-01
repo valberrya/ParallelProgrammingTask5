69ca22dbe7ad001d8ef5cf94ca94a18e51929e1f
valberrya <145595029+valberrya@users.noreply.github.com>
2023-11-02 02:40:45 +0500

    Add files via upload

---
+++ PPLab5CreateProcess/ChildProgram.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE 
+#include <stdio.h>
+#include <windows.h>
+
+const int imageSize = 1900;
+const int coefficient = 1;
+const int maxThreads = 8;
+const int threadStep = imageSize / maxThreads;
+
+void change_bytes(int start, int end, unsigned char buffer[]) {
+    for (int i = start; i > end; i -= threadStep) {
+        buffer[i] += (i * coefficient) & 255;
+        printf("%d ", (int)buffer[i]);
+    }
+}
+
+void program() {
+    unsigned char f[1900];
+    SIZE_T bytesRead;
+    ReadProcessMemory(GetCurrentProcess(), f, f, sizeof(f), &bytesRead);
+    change_bytes(imageSize - GetCurrentProcessId(), 0, f);
+    SIZE_T bytesWritten;
+    WriteProcessMemory(GetCurrentProcess(), f, f, sizeof(f), &bytesWritten);
+}
